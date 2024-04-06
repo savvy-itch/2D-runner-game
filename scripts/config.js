212a3f62e2fd7af19f90511c79dca92c2ccf173a
Mik-Halo <sprone3@gmail.com>
2024-04-06 21:36:40 +0300

    add obstacles

--- scripts/config.js
+++ scripts/config.js
@@ -3,3 +3,24 @@ export const spriteY = 128;
 export const idleVelocity = 13;
 export const jumpSprites = 7;
 export const offsetPerFrame = spriteY / Math.ceil(jumpSprites / 2);
+export const maxObstaclesPerScreen = 2;
+export const minDistanceBetweenObstacles = 50;
+export const chanceOfGroupedObstacles = 10;
+export const obstacles = [
+  {
+    imgSrc: "../images/cave_rock4.png",
+    sizeX: 63,
+    sizeY: 44,
+  },
+  {
+    imgSrc: "../images/cave_rock5.png",
+    sizeX: 29,
+    sizeY: 24,
+  },
+]
+
+// {
+//   imgSrc: "../images/cave_rock3.png",
+//   sizeX: 89,
+//   sizeY: 62
+// },
--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,15 @@
-import {spriteX, spriteY, idleVelocity, jumpSprites, offsetPerFrame} from './config.js';
+import {
+  spriteX, 
+  spriteY, 
+  idleVelocity, 
+  jumpSprites, 
+  offsetPerFrame,
+  obstacles,
+  maxObstaclesPerScreen,
+  minDistanceBetweenObstacles,
+  chanceOfGroupedObstacles
+} from './config.js';
+import {getRandom} from './helpers.js';
 
 const canvas = document.querySelector('.playfield');
 const width = canvas.width = 600;
@@ -6,59 +17,59 @@ const height = canvas.height = 400;
 const ctx = canvas.getContext('2d');
 
 ctx.fillStyle = 'beige';
-ctx.fillRect(0,0,width,height);
-ctx.translate(0, height / 2);
+ctx.fillRect(0, 0, width, height);
+ctx.translate(0, -height * 0.3);
+ctx.strokeRect(0, 0, width, height);
 
+let startGame = false; 
 let isJumpPressed = false;
+let isRunning = false;
 let sprite = 0;
 let frame = 0;
 let velocity = 5;
-let startGame = false;
+let loopId;
 
 const imageRun = new Image();
 imageRun.src = "../images/Run.png";
-let runId;
 document.addEventListener('keyup', (e) => {
   if (e.key === 'Enter' && !startGame) {
     startGame = true;
-    window.cancelAnimationFrame(idleId);
-    runId = window.requestAnimationFrame(drawRun);
+    isRunning = true;
   }
 });
 
 const imageIdle = new Image();
 imageIdle.src = "../images/Idle.png";
-let idleId;
-imageIdle.onload = () => {
-  idleId = window.requestAnimationFrame(drawIdle);
-};
 
 const imageJump = new Image();
 imageJump.src = "../images/Jump.png";
-let jumpId;
 document.addEventListener('keydown', (e) => {
   if ((e.code === 'Space' || e.code === 'ArrowUp') && startGame && !isJumpPressed) {
-    window.cancelAnimationFrame(runId);
     sprite = 0;
+    isRunning = false;
     isJumpPressed = true;
-    jumpId = window.requestAnimationFrame(drawJump);
   }
 });
 
+const imageRock = new Image();
+imageRock.src = "../images/cave_rock5.png";
+imageRock.onload = () => {
+  loop();
+};
+
 document.addEventListener('keyup', (e) => {
   if (e.code === 'Escape') {
-    console.log('abort')
-    window.cancelAnimationFrame(idleId);
-    window.cancelAnimationFrame(jumpId);
-    window.cancelAnimationFrame(runId);
+    console.log('abort');
+    window.cancelAnimationFrame(loopId);
+    isRunning = false;
+    isJumpPressed = false;
     startGame = false;
   }
-})
+});
+
+// =========== Character ===============
 
 function drawIdle() {
-  if (sprite === 0) console.log('drawIdle');
-
-  ctx.fillRect(-(width / 2), -(height / 2), width, height);
   ctx.drawImage(
     imageIdle, 
     sprite * spriteX, // top-left corner of the slice to cut out (X)
@@ -66,7 +77,7 @@ function drawIdle() {
     spriteX, // the size of the slice to cut out (X)
     spriteY, // the size of the slice to cut out (Y)
     0, // top-left corner of canvas box into which to draw the slice (X)
-    -64, // top-left corner of canvas box into which to draw the slice (Y)
+    height - spriteY, // top-left corner of canvas box into which to draw the slice (Y)
     128, // the size of the image on the canvas (X)
     128 // the size of the image on the canvas (Y)
   );
@@ -74,14 +85,9 @@ function drawIdle() {
   slowFramerate(idleVelocity, 4);
 
   frame++;
-
-  idleId = window.requestAnimationFrame(drawIdle);
 }
 
 function drawRun() {
-  if (sprite === 0) console.log('drawRun');
-
-  ctx.fillRect(-(width / 2), -(height / 2), width, height);
   ctx.drawImage(
     imageRun, 
     sprite * spriteX, // top-left corner of the slice to cut out (X)
@@ -89,7 +95,7 @@ function drawRun() {
     spriteX, // the size of the slice to cut out (X)
     spriteY, // the size of the slice to cut out (Y)
     0, // top-left corner of canvas box into which to draw the slice (X)
-    -64, // top-left corner of canvas box into which to draw the slice (Y)
+    height - spriteY, // top-left corner of canvas box into which to draw the slice (Y)
     128, // the size of the image on the canvas (X)
     128 // the size of the image on the canvas (Y)
   );
@@ -97,16 +103,14 @@ function drawRun() {
   slowFramerate(velocity, 7);
 
   frame++;
-
-  runId = window.requestAnimationFrame(drawRun);
 }
 
 let posY = 0;
 
 function drawJump() {
-  if (sprite === 0) console.log('drawJump');
+  // if (sprite === 0) console.log('drawJump');
 
-  ctx.fillRect(-(width / 2), -(height / 2), width, height);
+  // ctx.fillRect(-(width / 2), -(height / 2), width, height);
   ctx.drawImage(
     imageJump,
     sprite * spriteX, // top-left corner of the slice to cut out (X)
@@ -114,12 +118,12 @@ function drawJump() {
     spriteX, // the size of the slice to cut out (X)
     spriteY, // the size of the slice to cut out (Y)
     0, // top-left corner of canvas box into which to draw the slice (X)
-    -64 - posY, // top-left corner of canvas box into which to draw the slice (Y)
+    height - posY - spriteY, // top-left corner of canvas box into which to draw the slice (Y)
     128, // the size of the image on the canvas (X)
     128 // the size of the image on the canvas (Y)
   );
   
-  slowFramerate(velocity, jumpSprites);
+  slowFramerate(4, jumpSprites);
 
   // accending and descending
   if (sprite < Math.ceil(jumpSprites / 2)) {
@@ -128,7 +132,7 @@ function drawJump() {
     posY = (jumpSprites - sprite) * offsetPerFrame;
   }
 
-  // console.log(sprite, posY);
+  // console.log(sprite, posY, height - posY);
 
   frame++;
   
@@ -136,11 +140,8 @@ function drawJump() {
   if (sprite === jumpSprites) {
     sprite = 0;
     frame = 0;
-    window.cancelAnimationFrame(jumpId);
     isJumpPressed = false;
-    drawRun();
-  } else {
-    window.requestAnimationFrame(drawJump);
+    isRunning = true;
   }
 }
 
@@ -154,3 +155,107 @@ function slowFramerate(velocity, numOfSprites) {
     }
   }
 }
+
+// ============ Obstacles =================
+
+class Obstacle {
+  constructor(image, x, y, sizeX, sizeY, vel) {
+    this.image = image;
+    this.x = x;
+    this.y = y;
+    this.sizeX = sizeX;
+    this.sizeY = sizeY;
+    this.vel = vel;
+  }
+
+  drawRock() {
+    ctx.drawImage(
+      this.image, 
+      0, // top-left corner of the slice to cut out (X)
+      0, // top-left corner of the slice to cut out (Y)
+      this.sizeX, // the size of the slice to cut out (X)
+      this.sizeY, // the size of the slice to cut out (Y)
+      this.x, // top-left corner of canvas box into which to draw the slice (X)
+      this.y, // top-left corner of canvas box into which to draw the slice (Y)
+      this.sizeX, // the size of the image on the canvas (X)
+      this.sizeY // the size of the image on the canvas (Y)
+    );
+  }
+
+  updateRock() {
+    this.x -= this.vel;
+
+    // when an obstacle moved off-screen
+    if (this.x <= (0 - this.sizeX)) {
+      return true;
+      // obstacleArray.shift();
+      // createObstacles();
+    }
+
+    return false;
+  
+    // this.x -= this.vel;
+  }
+}
+
+function loop() {
+  ctx.fillRect(0, 0, width, height);
+
+  for (let i = 0; i < obstacleArray.length; i++) {
+    obstacleArray[i].drawRock();
+
+    if (isRunning || isJumpPressed) {
+      if (obstacleArray[i].updateRock()) {
+        obstacleArray.splice(i, 1);
+        i--;
+      }
+    }
+  }
+
+  if (isRunning) {
+    drawRun();
+  } else if (isJumpPressed) {
+    drawJump();
+  } else {
+    drawIdle();
+  }
+
+  loopId = window.requestAnimationFrame(loop);
+}
+
+let obstacleArray = [];
+let obstacleVelocity = 5;
+
+document.addEventListener('DOMContentLoaded', createObstacles);
+
+function createObstacles() {
+  obstacleArray = [];
+  let obstacleAmount = getRandom(0, maxObstaclesPerScreen);
+
+  for (let i = 0; i < obstacleAmount; i++) {
+
+    const obstacleObj = {};
+    const imageRock = new Image();
+    const idx = getRandom(0, obstacles.length - 1);
+    imageRock.src = obstacles[idx].imgSrc;
+    obstacleObj.x = width + getRandom(minDistanceBetweenObstacles, width);
+    obstacleObj.y = height - obstacles[idx].sizeY;
+    const obstacle = new Obstacle(imageRock, obstacleObj.x, obstacleObj.y, obstacles[idx].sizeX, obstacles[idx].sizeY, obstacleVelocity);
+    obstacleArray.push(obstacle);
+  }
+  console.log(obstacleAmount, obstacleArray);
+}
+
+
+// for (const item of obstacles) {
+//   const imageRock = new Image();
+//   imageRock.src = item.imgSrc;
+
+//   const obstacle = new Obstacle(imageRock, width + item.sizeX, 0, item.sizeX, item.sizeY, obstacleVelocity);
+//   obstacleArray.push(obstacle);
+// }
+
+// randomize position of an obstacle with regards to the distance to the next obstacle
+// add grouped obstacles probability
+
+// const obstacle = new Obstacle(imageRock, width + 29, 0, 29, 24, 5);
